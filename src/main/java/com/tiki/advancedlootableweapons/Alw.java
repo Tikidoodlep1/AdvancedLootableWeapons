980c565ef11650e8d0a77cd751941d3500bca98a
Tikidoodlep1 <alex.x.eye@gmail.com>
2022-06-15 14:28:34 -0500

    Added some intitial items

--- src/main/java/com/tiki/advancedlootableweapons/Alw.java
+++ src/main/java/com/tiki/advancedlootableweapons/Alw.java
@@ -1,21 +1,15 @@
 package com.tiki.advancedlootableweapons;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Block;
+import com.tiki.advancedlootableweapons.item.ItemInit;
+
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ModInfo.ID)
@@ -26,8 +20,11 @@ public class Alw
 
     public Alw()
     {
+    	IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        eventBus.addListener(this::setup);
+        
+        ItemInit.register(eventBus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
