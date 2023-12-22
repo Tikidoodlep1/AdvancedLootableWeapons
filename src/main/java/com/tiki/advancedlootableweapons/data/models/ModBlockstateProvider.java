abe821866366b79ce81efc1d8ef45c04edacaa46
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2023-12-22 15:30:17 -0500

    migrate item texture names

--- src/main/java/com/tiki/advancedlootableweapons/data/models/ModBlockstateProvider.java
+++ src/main/java/com/tiki/advancedlootableweapons/data/models/ModBlockstateProvider.java
@@ -1,6 +1,7 @@
 package com.tiki.advancedlootableweapons.data.models;
 
 import com.tiki.advancedlootableweapons.AdvancedLootableWeapons;
+import com.tiki.advancedlootableweapons.init.BlockInit;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
@@ -12,6 +13,6 @@ public class ModBlockstateProvider extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
-
+        ;
     }
 }
--- src/main/java/com/tiki/advancedlootableweapons/data/models/ModItemModelProvider.java
+++ src/main/java/com/tiki/advancedlootableweapons/data/models/ModItemModelProvider.java
@@ -2,9 +2,11 @@ package com.tiki.advancedlootableweapons.data.models;
 
 import com.tiki.advancedlootableweapons.AdvancedLootableWeapons;
 import com.tiki.advancedlootableweapons.init.BlockInit;
+import com.tiki.advancedlootableweapons.init.ItemInit;
 import net.minecraft.core.Registry;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackType;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
@@ -16,16 +18,38 @@ public class ModItemModelProvider extends ItemModelProvider {
 
     @Override
     protected void registerModels() {
-        makeSimpleBlockItem(BlockInit.BLOCK_FORGE.get().asItem());
+        simpleBlockItem(BlockInit.BLOCK_FORGE.get().asItem());
+
+        oneLayerItem(ItemInit.KOBOLD_INGOT.get());
+        oneLayerItem(ItemInit.PLATINUM_INGOT.get());
+        oneLayerItem(ItemInit.SILVER_INGOT.get());
+        oneLayerItem(ItemInit.STEEL_INGOT.get());
+        oneLayerItem(ItemInit.TIN_INGOT.get());
     }
 
-    protected void makeSimpleBlockItem(Item item, ResourceLocation loc) {
+    protected void oneLayerItem(Item item, ResourceLocation texture) {
+        String path = Registry.ITEM.getKey(item).getPath();
+        if (existingFileHelper.exists(new ResourceLocation(texture.getNamespace(), "item/" + texture.getPath())
+                , PackType.CLIENT_RESOURCES, ".png", "textures")) {
+            getBuilder(path).parent(getExistingFile(mcLoc("item/generated")))
+                    .texture("layer0", new ResourceLocation(texture.getNamespace(), "item/" + texture.getPath()));
+        } else {
+            System.out.println("no texture for " + item + " found, skipping");
+        }
+    }
+
+    protected void oneLayerItem(Item item) {
+        ResourceLocation texture = Registry.ITEM.getKey(item);
+        oneLayerItem(item, texture);
+    }
+
+    protected void simpleBlockItem(Item item, ResourceLocation loc) {
         String s = Registry.ITEM.getKey(item).toString();
         getBuilder(s).parent(getExistingFile(loc));
     }
 
-    protected void makeSimpleBlockItem(Item item) {
-        makeSimpleBlockItem(item, new ResourceLocation(AdvancedLootableWeapons.MODID, "block/" + Registry.ITEM.getKey(item).getPath()));
+    protected void simpleBlockItem(Item item) {
+        simpleBlockItem(item, new ResourceLocation(AdvancedLootableWeapons.MODID, "block/" + Registry.ITEM.getKey(item).getPath()));
     }
 
 
