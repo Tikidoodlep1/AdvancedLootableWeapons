ad4080c02b069cc9a72b7768e1cbca0f0931cd5b
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2023-12-24 00:01:41 -0500

    fix armor material configs

--- src/main/java/com/tiki/advancedlootableweapons/items/armor/ArmorAttributes.java
+++ src/main/java/com/tiki/advancedlootableweapons/items/armor/ArmorAttributes.java
@@ -10,6 +10,7 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeConfigSpec;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Supplier;
@@ -17,37 +18,37 @@ import java.util.function.Supplier;
 @SuppressWarnings("deprecation")
 public enum ArmorAttributes implements ArmorMaterial {
 
-    KOBOLD("kobold", new int[]{1, 3, 4, 2}, CommonConfigHandler.KOBOLD_ARMOR_DURABILITY.get(), 22, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.KOBOLD_ARMOR_HARDNESS.get(), 0.0F, () -> {
+    KOBOLD("kobold", new int[]{1, 3, 4, 2}, CommonConfigHandler.KOBOLD_ARMOR_DURABILITY, 22, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.KOBOLD_ARMOR_HARDNESS, 0.0F, () -> {
         return Ingredient.of(ItemInit.KOBOLD_INGOT.get());
     }),
-    COPPER("copper", new int[]{2, 3, 4, 2}, CommonConfigHandler.COPPER_ARMOR_DURABILITY.get(), 10, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.COPPER_ARMOR_HARDNESS.get(), 0.0F, () -> {
+    COPPER("copper", new int[]{2, 3, 4, 2}, CommonConfigHandler.COPPER_ARMOR_DURABILITY, 10, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.COPPER_ARMOR_HARDNESS, 0.0F, () -> {
         return Ingredient.of(Items.COPPER_INGOT);
     }),
-    SILVER("silver", new int[]{3, 5, 7, 3}, CommonConfigHandler.SILVER_ARMOR_DURABILITY.get(), 24, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.SILVER_ARMOR_HARDNESS.get(), 0.0F, () -> {
+    SILVER("silver", new int[]{3, 5, 7, 3}, CommonConfigHandler.SILVER_ARMOR_DURABILITY, 24, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.SILVER_ARMOR_HARDNESS, 0.0F, () -> {
         return Ingredient.of(ItemInit.SILVER_INGOT.get());
     }),
-    BRONZE("bronze", new int[]{5, 9, 12, 5}, CommonConfigHandler.BRONZE_ARMOR_DURABILITY.get(), 12, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.BRONZE_ARMOR_HARDNESS.get(), 0.0F, () -> {
+    BRONZE("bronze", new int[]{5, 9, 12, 5}, CommonConfigHandler.BRONZE_ARMOR_DURABILITY, 12, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.BRONZE_ARMOR_HARDNESS, 0.0F, () -> {
         return Ingredient.of(ItemInit.BRONZE_INGOT.get());
     }),
-    PLATINUM("platinum", new int[]{7, 13, 17, 8}, CommonConfigHandler.PLATINUM_ARMOR_DURABILITY.get(), 26, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.PLATINUM_ARMOR_HARDNESS.get(), 0.0F, () -> {
+    PLATINUM("platinum", new int[]{7, 13, 17, 8}, CommonConfigHandler.PLATINUM_ARMOR_DURABILITY, 26, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.PLATINUM_ARMOR_HARDNESS, 0.0F, () -> {
         return Ingredient.of(ItemInit.PLATINUM_INGOT.get());
     }),
-    STEEL("steel", new int[]{3, 5, 6, 3}, CommonConfigHandler.STEEL_ARMOR_DURABILITY.get(), 18, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.STEEL_ARMOR_HARDNESS.get(), 0.0F, () -> {
+    STEEL("steel", new int[]{3, 5, 6, 3}, CommonConfigHandler.STEEL_ARMOR_DURABILITY, 18, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.STEEL_ARMOR_HARDNESS, 0.0F, () -> {
         return Ingredient.of(ItemInit.STEEL_INGOT.get());
     }),
-    SHADOW_PLATINUM("shadow_platinum", new int[]{7, 13, 16, 7}, CommonConfigHandler.SHADOW_PLATINUM_ARMOR_DURABILITY.get(), 21, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.SHADOW_PLATINUM_ARMOR_HARDNESS.get(), 0.0F, () -> {
+    SHADOW_PLATINUM("shadow_platinum", new int[]{7, 13, 16, 7}, CommonConfigHandler.SHADOW_PLATINUM_ARMOR_DURABILITY, 21, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.SHADOW_PLATINUM_ARMOR_HARDNESS, 0.0F, () -> {
         return Ingredient.of(ItemInit.SHADOW_PLATINUM_INGOT.get());
     }),
-    FROST_STEEL("frost_steel", new int[]{4, 7, 9, 4}, CommonConfigHandler.FROST_STEEL_ARMOR_DURABILITY.get(), 30, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.FROST_STEEL_ARMOR_HARDNESS.get(), 0.0F, () -> {
+    FROST_STEEL("frost_steel", new int[]{4, 7, 9, 4}, CommonConfigHandler.FROST_STEEL_ARMOR_DURABILITY, 30, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.FROST_STEEL_ARMOR_HARDNESS, 0.0F, () -> {
         return Ingredient.of(ItemInit.FROST_STEEL_INGOT.get());
     }),
-    OBSIDIAN_STEEL("obsidian_steel", new int[]{3, 5, 6, 3}, CommonConfigHandler.OBSIDIAN_ARMOR_DURABILITY.get(), 18, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.OBSIDIAN_ARMOR_HARDNESS.get(), 0.0F, () -> {
+    OBSIDIAN_STEEL("obsidian_steel", new int[]{3, 5, 6, 3}, CommonConfigHandler.OBSIDIAN_ARMOR_DURABILITY, 18, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.OBSIDIAN_ARMOR_HARDNESS, 0.0F, () -> {
         return Ingredient.of(ItemInit.REFINED_OBSIDIAN_INGOT.get());
     }),
-    CRYSTALLITE("crystallite", new int[]{5, 10, 13, 6}, CommonConfigHandler.CRYSTALLITE_ARMOR_DURABILITY.get(), 20, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.CRYSTALLITE_ARMOR_HARDNESS.get(), 0.0F, () -> {
+    CRYSTALLITE("crystallite", new int[]{5, 10, 13, 6}, CommonConfigHandler.CRYSTALLITE_ARMOR_DURABILITY, 20, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.CRYSTALLITE_ARMOR_HARDNESS, 0.0F, () -> {
         return Ingredient.of(ItemInit.CRYSTALLITE_INGOT.get());
     }),
-    DUSK_STEEL("dusk_steel", new int[] {5, 8, 11, 5}, CommonConfigHandler.DUSKSTEEL_ARMOR_DURABILITY.get(), 14, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.DUSKSTEEL_ARMOR_HARDNESS.get(), 0.0F, () -> {
+    DUSK_STEEL("dusk_steel", new int[] {5, 8, 11, 5}, CommonConfigHandler.DUSKSTEEL_ARMOR_DURABILITY, 14, SoundEvents.ARMOR_EQUIP_IRON, CommonConfigHandler.DUSKSTEEL_ARMOR_HARDNESS, 0.0F, () -> {
         return Ingredient.of(ItemInit.DUSKSTEEL_INGOT.get());
     });
 
@@ -56,19 +57,21 @@ public enum ArmorAttributes implements ArmorMaterial {
     private static final float LEGGINGS_DURABILITY_OFFSET = 0.9F;
     private static final float BOOTS_DURABILITY_OFFSET = 0.7F;
 
+    private static final float[] slotDurMultipliers = new float[]{HELMET_DURABILITY_OFFSET,CHEST_DURABILITY_OFFSET,LEGGINGS_DURABILITY_OFFSET,BOOTS_DURABILITY_OFFSET};
+
     private final String name;
     private final int[] slotProtections;
-    private final float[] healthPerSlot;
     private final int enchantmentValue;
     private final SoundEvent sound;
-    private final float toughness;
+    private final ForgeConfigSpec.DoubleValue toughness;
     private final float knockbackResistance;
     private final LazyLoadedValue<Ingredient> repairIngredient;
+    private final ForgeConfigSpec.IntValue healthPerSlot;
 
-    ArmorAttributes(String name, int[] damageReductionAmount, float healthPerSlot, int enchantability, SoundEvent sound, float toughness, float knockbackResistance, Supplier<Ingredient> repairMaterial) {
+    ArmorAttributes(String name, int[] damageReductionAmount, ForgeConfigSpec.IntValue healthPerSlot, int enchantability, SoundEvent sound, ForgeConfigSpec.DoubleValue toughness, float knockbackResistance, Supplier<Ingredient> repairMaterial) {
         this.name = name;
         this.slotProtections = damageReductionAmount;
-        this.healthPerSlot = new float[] {healthPerSlot * HELMET_DURABILITY_OFFSET, healthPerSlot * CHEST_DURABILITY_OFFSET, healthPerSlot * LEGGINGS_DURABILITY_OFFSET, healthPerSlot * BOOTS_DURABILITY_OFFSET};
+        this.healthPerSlot = healthPerSlot;
         this.enchantmentValue = enchantability;
         this.sound = sound;
         this.toughness = toughness;
@@ -96,7 +99,8 @@ public enum ArmorAttributes implements ArmorMaterial {
 
     @Override
     public int getDurabilityForSlot(EquipmentSlot type) {
-        return (int) this.healthPerSlot[type.getIndex()];
+        int base = healthPerSlot.get();
+        return (int) (base * slotDurMultipliers[type.getIndex()]);
     }
 
     @Override
@@ -111,7 +115,7 @@ public enum ArmorAttributes implements ArmorMaterial {
 
     @Override
     public float getToughness() {
-        return this.toughness;
+        return (float)(double)this.toughness.get();
     }
 
     @Override
