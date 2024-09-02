3509960853f8dee0f5eb0eab2703207730062f54
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2024-09-01 23:33:46 -0400

    improve bar rendering

--- src/main/java/com/tiki/advancedlootableweapons/inventory/advanced_forge/AdvancedForgeScreen.java
+++ src/main/java/com/tiki/advancedlootableweapons/inventory/advanced_forge/AdvancedForgeScreen.java
@@ -3,6 +3,7 @@ package com.tiki.advancedlootableweapons.inventory.advanced_forge;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.tiki.advancedlootableweapons.AdvancedLootableWeapons;
+import com.tiki.advancedlootableweapons.blocks.block_entity.AdvancedForgeBlockEntity;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
@@ -16,6 +17,7 @@ public class AdvancedForgeScreen extends AbstractContainerScreen<AdvancedForgeCo
 
 	static final int BAR_X_OFFSET = 135;
 	static final int BAR_WIDTH = 19;
+	static final int BAR_HEIGHT = 65;
 
 	public AdvancedForgeScreen(AdvancedForgeContainer pMenu, Inventory pPlayerInventory, Component pTitle) {
 		super(pMenu, pPlayerInventory, pTitle);
@@ -31,9 +33,8 @@ public class AdvancedForgeScreen extends AbstractContainerScreen<AdvancedForgeCo
 		int y = (height - imageHeight) / 2;
 		
 		this.blit(pPoseStack, x, y, 0, 0, imageWidth, imageHeight);
-		
-		int temp = (int)(menu.getContainerTemp() * 0.037);
-		this.blit(pPoseStack, this.getGuiLeft() + BAR_X_OFFSET, this.getGuiTop() + 69 - temp, 176, 65 - temp, 18, temp);
+		double progress = (double) (BAR_HEIGHT * (menu.getContainerTemp() - AdvancedForgeBlockEntity.MIN_TEMP)) / (AdvancedForgeBlockEntity.MAX_TEMP - AdvancedForgeBlockEntity.MIN_TEMP);
+		this.blit(pPoseStack, this.getGuiLeft() + BAR_X_OFFSET, (int) (this.getGuiTop() + 69 - progress), 176, (int) (BAR_HEIGHT - progress), 18, (int) progress);
 	}
 	
 	@Override
--- src/main/java/com/tiki/advancedlootableweapons/inventory/forge/ForgeScreen.java
+++ src/main/java/com/tiki/advancedlootableweapons/inventory/forge/ForgeScreen.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.tiki.advancedlootableweapons.AdvancedLootableWeapons;
 
+import com.tiki.advancedlootableweapons.blocks.block_entity.AdvancedForgeBlockEntity;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
@@ -14,6 +15,10 @@ import net.minecraft.world.entity.player.Inventory;
 public class ForgeScreen extends AbstractContainerScreen<ForgeContainer> {
 
 	private static final ResourceLocation TEXTURE = AdvancedLootableWeapons.id( "textures/gui/forge.png");
+
+	static final int BAR_X_OFFSET = 122;
+	static final int BAR_WIDTH = 19;
+	static final int BAR_HEIGHT = 65;
 
 	public ForgeScreen(ForgeContainer pMenu, Inventory pPlayerInventory, Component pTitle) {
 		super(pMenu, pPlayerInventory, pTitle);
@@ -29,9 +34,9 @@ public class ForgeScreen extends AbstractContainerScreen<ForgeContainer> {
 		int y = (height - imageHeight) / 2;
 		
 		this.blit(pPoseStack, x, y, 0, 0, imageWidth, imageHeight);
-		
-		int temp = (int)(menu.getContainerTemp() * 0.037);
-		this.blit(pPoseStack, this.getGuiLeft() + 122, this.getGuiTop() + 69 - temp, 176, 65 - temp, 18, temp);
+
+		double progress = (double) (BAR_HEIGHT * (menu.getContainerTemp() - AdvancedForgeBlockEntity.MIN_TEMP)) / (AdvancedForgeBlockEntity.MAX_TEMP - AdvancedForgeBlockEntity.MIN_TEMP);
+		this.blit(pPoseStack, this.getGuiLeft() + BAR_X_OFFSET, (int) (this.getGuiTop() + 69 - progress), 176, (int) (BAR_HEIGHT - progress), 18, (int) progress);
 	}
 	
 	@Override
@@ -39,7 +44,7 @@ public class ForgeScreen extends AbstractContainerScreen<ForgeContainer> {
 		renderBackground(pPoseStack);
 		super.render(pPoseStack, pMouseX, pMouseY, pPartialTick);
 		renderTooltip(pPoseStack, pMouseX, pMouseY);
-		if(pMouseX > this.getGuiLeft() + 122 && pMouseX < this.getGuiLeft() + 141 && pMouseY > this.getGuiTop() + 4 && pMouseY < this.getGuiTop() + 70) {
+		if(pMouseX > this.getGuiLeft() + BAR_X_OFFSET && pMouseX < this.getGuiLeft() + BAR_X_OFFSET + BAR_WIDTH && pMouseY > this.getGuiTop() + 4 && pMouseY < this.getGuiTop() + 70) {
 			double temp = ((menu.getContainerTemp()-32)*5/9);
 			this.renderTooltip(pPoseStack, new TextComponent("Forge Temperature: " + (int)temp + " Celcius"), pMouseX, pMouseY);
 		}
