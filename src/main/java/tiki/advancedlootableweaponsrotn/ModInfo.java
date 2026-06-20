6ae64cf2417a25ae6f076cadbc973df49150e5f1
Tikidoodlep1 <alex.x.eye@gmail.com>
2026-06-20 13:01:32 -0500

    updated ALW verison used

--- src/main/java/tiki/advancedlootableweaponsrotn/ModInfo.java
+++ src/main/java/tiki/advancedlootableweaponsrotn/ModInfo.java
@@ -4,7 +4,7 @@ public class ModInfo {
 	public static final String ID = "advancedlootableweaponsrotn";
 	public static final String NAME = "Advanced Lootable Weapons RotN";
 	public static final String VER = "0.1.0-1.12.2";
-	public static final String LOAD_AFTER = "required-after:advancedlootableweapons@[1.4.6.5,)";
+	public static final String LOAD_AFTER = "required-after:advancedlootableweapons@[1.4.6.6,)";
 	public static final String CLIENTPROXY = "tiki.advancedlootableweaponsrotn.proxy.ClientProxy";
 	public static final String COMMONPROXY = "tiki.advancedlootableweaponsrotn.proxy.CommonProxy";
 }
